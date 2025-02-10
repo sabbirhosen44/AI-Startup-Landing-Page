610113576792c0f882c5d9fcf680c80f0e57af9e
Jawwad926 <mdsabbirhosen926@gmail.com>
2025-02-10 06:18:48 +0600

    feat: add Hero secion

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,3 +1,25 @@
+import Button from "@/components/Button";
+import starsBG from "@/assets/stars.png";
+
 export const Hero = () => {
-  return <section>Hero</section>;
+  return (
+    <section
+      className="h-[492px] flex items-center"
+      style={{ backgroundImage: `url(${starsBG.src})` }}
+    >
+      <div className="absolute h-64 w-64 bg-purple-500 rounded-full border border-white/30 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-[radial-gradient(50%_50%_at_16.8%_18.3%,white,rgb(184,148,255)_37.7%,rgb(24,0,66))]" />
+      <div className="container relative">
+        <h1 className="text-8xl font-semibold tracking-tighter bg-white bg-[radial-gradient(100%_100%_at_top_left,white,white,rgb(74,32,138,.5))] text-transparent bg-clip-text text-center">
+          AI SEO
+        </h1>
+        <p className="text-lg text-white/70 mt-5 text-center">
+          Elevate your site's visibility effortlessly with AI, where smart
+          technology meets user-friendly SEO tools.
+        </p>
+        <div className="flex justify-center mt-5">
+          <Button>Join Waitlist</Button>
+        </div>
+      </div>
+    </section>
+  );
 };
