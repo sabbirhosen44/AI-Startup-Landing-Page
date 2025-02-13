019294219c7f8bbfc2fc870e95422175c143aeb9
Jawwad926 <mdsabbirhosen926@gmail.com>
2025-02-13 16:21:22 +0600

    feat: add the feature section

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -4,9 +4,11 @@ import Button from "@/components/Button";
 
 export const Header = () => {
   return (
-    <header className="py-4 border-b border-b-white/15 md:border-none sticky top-0 z-10 backdrop-blur md:backdrop-blur-none">
+    <header className="py-4 border-b border-b-white/15 md:border-none sticky top-0 z-10 ">
+      <div className="absolute inset-0 backdrop-blur -z-10 md:hidden"></div>
       <div className="container">
-        <div className="flex justify-between items-center md:border border-white/15 md:p-2.5 rounded-xl max-w-2xl mx-auto md:backdrop-blur">
+        <div className="flex justify-between items-center md:border border-white/15 md:p-2.5 rounded-xl max-w-2xl mx-auto relative">
+          <div className="absolute inset-0 backdrop-blur -z-10 hidden md:block"></div>
           <div className="border h-10 w-10 inline-flex rounded-lg justify-center items-center border-white/15">
             <LogoIcon className="h-8 w-8" />
           </div>
