2758a962e8b50c1e6515e02cb6911397985fbb83
Jawwad926 <mdsabbirhosen926@gmail.com>
2025-02-21 23:44:38 +0600

    feat: add parallax effect to the hero section

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,13 +1,37 @@
 "use client";
-import Button from "@/components/Button";
 import starsBG from "@/assets/stars.png";
-import { motion } from "framer-motion";
+import Button from "@/components/Button";
+import {
+  motion,
+  useMotionValueEvent,
+  useScroll,
+  useTransform,
+} from "framer-motion";
+import { useRef } from "react";
 
 export const Hero = () => {
+  const sectionRef = useRef(null);
+
+  const { scrollYProgress } = useScroll({
+    target: sectionRef,
+    offset: ["start end", "end start"],
+  });
+
+  // useMotionValueEvent(scrollYProgress, "change", (value) => {
+  //   console.log("scrollYProgress", value);
+  // });
+
+  const backgroundPositionY = useTransform(
+    scrollYProgress,
+    [0, 1],
+    [-300, 300]
+  );
+
   return (
     <motion.section
+      ref={sectionRef}
       className="h-[492px] md:h-[800px] flex items-center overflow-hidden relative [mask-image:linear-gradient(to_bottom,transparent,black_10%,black_90%,transparent)]"
-      style={{ backgroundImage: `url(${starsBG.src})` }}
+      style={{ backgroundImage: `url(${starsBG.src})`, backgroundPositionY }}
       animate={{
         backgroundPositionX: starsBG.width,
       }}
